9c5713c7160246be5f14229abd4c672bb08b5773
M Somerville <matthew-github@dracos.co.uk>
2021-06-21 17:43:50 +0100

    Add case reassigning.

--- cases/urls.py
+++ cases/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.case_list, name="cases"),
     path("/<int:pk>", views.case, name="case-view"),
+    path("/<int:pk>/reassign", views.reassign, name="case-reassign"),
 ]
