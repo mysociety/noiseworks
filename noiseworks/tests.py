80323715a10993d5db211ba64db7656c252d8d2f
M Somerville <matthew-github@dracos.co.uk>
2021-07-07 16:21:15 +0100

    Stand-in home page.

--- noiseworks/tests.py
+++ noiseworks/tests.py
@@ -3,4 +3,4 @@ from pytest_django.asserts import assertContains
 
 def test_with_client(client):
     response = client.get("/")
-    assertContains(response, "Not Found", status_code=404)
+    assertContains(response, "In progress home page")
