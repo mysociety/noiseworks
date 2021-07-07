f4c607cb37cf0b349ec9bf3e2f89bea1ce7613cc
M Somerville <matthew-github@dracos.co.uk>
2021-07-07 16:21:15 +0100

    Stand-in home page.

--- noiseworks/urls.py
+++ noiseworks/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import include, path
 import debug_toolbar
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="home.html")),
     path("cases", include("cases.urls")),
     path("oauth/", include("oauth.urls")),
     path("admin/", admin.site.urls),
