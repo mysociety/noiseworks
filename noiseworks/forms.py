e61ce4e23a1b61ed6fc84334a82cfa90f8fa4edf
M Somerville <matthew-github@dracos.co.uk>
2021-07-08 12:19:18 +0100

    Factor out GDSForm.

---
+++ noiseworks/forms.py
@@ -0,0 +1,14 @@
+from crispy_forms_gds.helper import FormHelper
+from crispy_forms_gds.layout import Submit
+
+
+class GDSForm:
+    """Mixin to add a submit button to the form"""
+
+    submit_text = "Submit"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+        self.helper.add_input(Submit("", self.submit_text))
