8cef3a8192897c6ea0aee3dc255f5159f57ebbbf
Dave Arter <davea@mysociety.org>
2021-07-14 14:43:03 +0100

    Correct value in og:url tag

--- noiseworks/tests.py
+++ noiseworks/tests.py
@@ -4,3 +4,6 @@ from pytest_django.asserts import assertContains
 def test_with_client(client):
     response = client.get("/")
     assertContains(response, "In progress home page")
+    assertContains(
+        response, """<meta property="og:url" content="http://testserver/">"""
+    )
