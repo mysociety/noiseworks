880dc4ddecd471f94825a29927e7b8c658a5266d
M Somerville <matthew-github@dracos.co.uk>
2022-01-12 22:58:29 +0000

    Upgrade Django, certifi, crispy-forms-gds.

--- noiseworks/forms.py
+++ noiseworks/forms.py
@@ -1,22 +1,5 @@
-from datetime import date
-from django.core.validators import ValidationError
 from crispy_forms_gds.helper import FormHelper
-from crispy_forms_gds.fields import DateInputField
 from crispy_forms_gds.layout import Submit
-
-
-def monkeypatch(self, data_list):
-    day, month, year = data_list
-    if day and month and year:
-        try:
-            return date(day=int(day), month=int(month), year=int(year))
-        except ValueError as e:
-            raise ValidationError(str(e)) from e
-    else:  # pragma: no cover
-        return None
-
-
-DateInputField.compress = monkeypatch
 
 
 class GDSForm:
