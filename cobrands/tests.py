543479a80c212d3cdcdc8eb9a1c2ddd9c67f4593
Nik Gupta <vngupta77@gmail.com>
2026-09-19 13:34:31 +0100

    Add test case to ensure cobrand templates are 'hooks' only.
    Potentially something we'd want to revisit but for now ensures
    we're not faliing to override something when we expected to.

--- cobrands/tests.py
+++ cobrands/tests.py
@@ -1,6 +1,21 @@
+from django.conf import settings
+
 from .interface import Cobrand
 from .testing import TestCobrand
 
 
 def test_test_cobrand_implements_cobrand() -> None:
     _: Cobrand = TestCobrand()
+
+
+def test_cobrand_templates_are_all_hooks() -> None:
+    """Fails if a cobrand defines a template that isn't a 'hook'
+    - a template we explicitly allow cobrands to override."""
+    cobrands = settings.BASE_DIR / "cobrands"
+    defaults = cobrands / "defaults" / "templates"
+    for templates in cobrands.glob("*/templates"):
+        for path in templates.rglob("*"):
+            if path.is_file():
+                relative = path.relative_to(templates)
+                assert relative.parts[0] == "cobrand", path
+                assert (defaults / relative).is_file(), path
